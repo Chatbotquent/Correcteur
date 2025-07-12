d32f25e32de6fd4ea706456fd0a6f7dd2a94b6ac
Chatbotquent <vaxelaire.quentin.3b@gmail.com>
2025-07-13 01:05:30 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify, send_from_directory
+import language_tool_python
+
+app = Flask(__name__, static_folder='static')
+tool = language_tool_python.LanguageTool('fr')
+
+@app.route('/')
+def index():
+    return send_from_directory('static', 'index.html')
+
+@app.route('/corriger', methods=['POST'])
+def corriger():
+    data = request.get_json()
+    texte = data.get('texte', '')
+
+    matches = tool.check(texte)
+    texte_corrige = language_tool_python.utils.correct(texte, matches)
+
+    texte_souligne = texte
+    offset = 0
+    details = []
+
+    for m in matches:
+        start = m.offset + offset
+        end = start + m.errorLength
+        correction = m.replacements[0] if m.replacements else ''
+        texte_souligne = (texte_souligne[:start] +
+                          "<u style='color:red'>" + texte_souligne[start:end] + "</u>" +
+                          texte_souligne[end:])
+        offset += len("<u style='color:red'></u>")
+
+        details.append({
+            "erreur": texte[m.offset:m.offset+m.errorLength],
+            "suggestion": correction,
+            "explication": m.message
+        })
+
+    return jsonify({
+        "original": texte,
+        "corrige": texte_corrige,
+        "avec_soulignage": texte_souligne,
+        "details": details
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
